423d7d4c3c49e956994198e9f87d5dfa3537f53d
roxywasiak <ruksclone@hotmail.com>
2022-04-24 16:59:11 +0100

    added some functionality to the funtions and tested the triangle validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,20 @@ const getSide = (message) => {
 
 //declare function validateSides
 
-const validateSides = () => {};
+const validateSides = (sideA, sideB, sideC) => {
+  //check if all three numbers are present
+  const allSidesDefined = sideA && sideB && sideC;
+
+  // check if all number > than 0
+  const allSidesGreaterThanZero = sideA > 0 && sideB > 0 && sideC > 0;
+
+  //check for triangle inequality
+  const inequalityCheck =
+    sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC > sideB;
+
+  //return true for all three checks
+  return allSidesDefined && allSidesGreaterThanZero && inequalityCheck;
+};
 
 //declare function triangle
 const triangle = () => {};
@@ -32,8 +45,14 @@ const sideC = getSide("Please enter sideC?");
 console.log(sideC);
 
 //validate side A,side B,side C
+const isValid = validateSides(sideA, sideB, sideC);
 
 //if invalid present a message
+if (isValid) {
+  console.log("yippee");
+} else {
+  alert("The triangle is invalid");
+}
 //if valid then continue
 
 //check for triangle type
