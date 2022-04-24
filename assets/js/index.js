206f81070306880ecefaa1d21e96f1f422200f39
Abdirahman Osman <abdiosman779@gmail.com>
2022-04-24 15:53:46 +0100

    functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,31 @@
-console.log("hello");
-
 //declare function getSide
+const getSide = (message) => {
+  return 5;
+};
 
 //declare function validateSides
 
-//declare function triangle
+const validateSides = () => {};
 
+//declare function triangle
+const triangle = () => {};
 
 //declare variable side A
+const sideA = getSide("Please enter sideA?");
+console.log(sideA);
 
 //declare variable side B
+const sideB = getSide("Please enter sideB?");
+console.log(sideB);
 
 //declare variable side C
+const sideC = getSide("Please enter sideC?");
+console.log(sideC);
 
 //validate side A,side B,side C
 
-//if invalid present a message 
+//if invalid present a message
 //if valid then continue
 
-//check for triangle type 
+//check for triangle type
 //display message
-
