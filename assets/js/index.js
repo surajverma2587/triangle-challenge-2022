5913b5d58d8fc24be7ff42d68f71ffea874daaa4
Abdirahman Osman <abdiosman779@gmail.com>
2022-04-24 15:40:08 +0100

    second-commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,23 @@
 console.log("hello");
 
+//declare function getSide
+
+//declare function validateSides
+
+//declare function triangle
+
+
+//declare variable side A
+
+//declare variable side B
+
+//declare variable side C
+
+//validate side A,side B,side C
+
+//if invalid present a message 
+//if valid then continue
+
+//check for triangle type 
+//display message
+
